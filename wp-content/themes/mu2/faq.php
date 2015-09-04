42d1c7055a80224b1baa86b97ee680a9d15f2406
Derek Potts <derek@2020creative.com>
2015-09-04 16:37:17 -0500

    faq UI enhancements

--- wp-content/themes/mu2/faq.php
+++ wp-content/themes/mu2/faq.php
@@ -5,21 +5,30 @@ get_header();
 query_posts( [ 'posts_per_page' => -1, 'post_type' => 'faq', 'orderby' => 'title', 'order' => 'ASC' ] );
 ?>
 
-<main>
-    <?php while ( have_posts() ) : the_post(); ?>
-        <div class="row">
+<main style="padding-top: 2rem; padding-bottom: 4rem;">
+    <?php
+        $post_seq_num = 0;
+        while ( have_posts() ) : the_post();
+        $post_seq_num++;
+        $answer_id = 'faq_' . $post_seq_num;
+    ?>
+        <div class="row faq-button" data-toggle="collapse" data-target="#<?php echo $answer_id; ?>">
+            <!--
             <div class="col-xs-2">
                 <h3 style="text-align: right;">Q:</h3>
             </div>
-            <div class="col-xs-10">
+            -->
+            <div class="col-xs-12">
                 <h3><?php the_title(); ?></h3>
             </div>
         </div>
-        <div class="row">
+        <div id="<?php echo $answer_id; ?>" class="row collapse">
+            <!--
             <div class="col-xs-2">
                 <h3 style="text-align: right; margin-top: initial;">A:</h3>
             </div>
-            <div class="col-xs-10">
+            -->
+            <div class="col-xs-12">
                 <?php the_content(); ?>
             </div>
         </div>
