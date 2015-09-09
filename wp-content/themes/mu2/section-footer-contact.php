a140f4b67a25011e930f3f7d1506abba35e0cd67
Jeff Potts <jeff@2020creative.com>
2015-09-09 11:42:44 -0500

    content wip

--- wp-content/themes/mu2/section-footer-contact.php
+++ wp-content/themes/mu2/section-footer-contact.php
@@ -1,18 +1,32 @@
 <div class="row footer-contact-container">
-    <div class="col-xs-10 col-xs-offset-1">
+    <div class="col-xs-12">
 
         <div class="row">
 
-            <div class="col-sm-4 footer-text-widget">
-                <?php dynamic_sidebar('tt-sidebar-footer-left'); ?>
+            <div class="col-sm-3 footer-text-widget">
+	            <h2>Sponsorships Available Now</h2>
+	            
+	            <a href="/pricing">Pricing</a></br>
+	            <a href="/sponsorship">Registration</a>
+	            <h4>Promote your business</h4>
+                <?php //dynamic_sidebar('tt-sidebar-footer-left'); ?>
             </div>
 
-            <div class="col-sm-4 footer-text-widget center">
-                <?php dynamic_sidebar('tt-sidebar-footer-center'); ?>
+            <div class="col-sm-6 footer-text-widget center">
+                <h2>Newsletter Archive</h2>
+                <a href="#">Sep 2015</a></br>
+                <a href="#">Oct 2015</a></br>
+                <a href="#">Nov 2015</a></br>
+                <?php //dynamic_sidebar('tt-sidebar-footer-center'); ?>
             </div>
 
-            <div class="col-sm-4 footer-text-widget">
-                <?php dynamic_sidebar('tt-sidebar-footer-right'); ?>
+            <div class="col-sm-3 footer-text-widget">
+	            <h3>Muscle Metrics LLC.</h3>
+				<p>15202 Catalina St.</br>
+				Leawood, KS 66224</p>
+				<h3>913-905-9789</h3>
+                <a href="/contact"><h4><i class="fa fa-envelope"></i> Email us</h4></a>
+                <?php //dynamic_sidebar('tt-sidebar-footer-right'); ?>
             </div>
 
         </div>
