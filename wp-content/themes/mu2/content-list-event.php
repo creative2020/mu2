356f51a52629e50f7bb5bd8b451adadec23ec2b6
Derek Potts <derek@2020creative.com>
2015-09-15 14:15:40 -0500

    register button points to post's page

--- wp-content/themes/mu2/content-list-event.php
+++ wp-content/themes/mu2/content-list-event.php
@@ -30,7 +30,7 @@
 		        </div>
 	        </div>
 			<div id="tt-event-btn" class="col-sm-12">
-				<?php echo do_shortcode('[tt_btn link="#" block="y"]Register[/tt_btn]'); ?>
+				<?php echo do_shortcode('[tt_btn link="'.$permalink.'" block="y"]Register[/tt_btn]'); ?>
 			</div>
 	        
 	    </div>
